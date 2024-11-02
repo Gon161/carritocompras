4da76eb9da2a58d5d184e33d3408223819fa29eb
Gon161 <nerialvareze@gmail.com>
2024-11-02 10:11:30 -0600

    migracion

--- database/migrations/2024_10_28_015524_create_usuarios_table.php
+++ database/migrations/2024_10_28_015524_create_usuarios_table.php
@@ -15,6 +15,19 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('paterno');
+            $table->string('materno');
+            $table->string('telefono',10);
+            $table->string('correo');
+            $table->string('password');
+            $table->text('token_recovery')->nullable();
+            $table->dateTime('nacimiento');
+
+            $table->foreign('id_Direccion')->references('id')->on('direcciones')->onDelete('set null')->nullable();
+            $table->foreign('id_Tarjeta')->references('id')->on('Tarjetas')->onDelete('set null')->nullable();
+            
+            $table->timestamps();
             $table->timestamps();
         });
     }
